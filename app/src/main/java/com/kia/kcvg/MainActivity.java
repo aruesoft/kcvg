18dc77e12b094d35ab5b95e4c1c12e4812959cae
jinhwan choi <jinhwan.stephen.choi@gmail.com>
2017-06-20 07:43:12 +0900

    Add top toolbar, bottom footer

--- app/src/main/java/com/kia/kcvg/MainActivity.java
+++ app/src/main/java/com/kia/kcvg/MainActivity.java
@@ -2,8 +2,13 @@ package com.kia.kcvg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.webview) WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
