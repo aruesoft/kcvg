96c7af392589ed3b5cb2ddd4bd9198e21904ca4a
jinhwan choi <jinhwan.stephen.choi@gmail.com>
2017-06-20 07:55:33 +0900

    Connect webview with developement webpage

--- app/src/main/java/com/kia/kcvg/MainActivity.java
+++ app/src/main/java/com/kia/kcvg/MainActivity.java
@@ -1,10 +1,12 @@
 package com.kia.kcvg;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://kcvgapp.jjworxdns.com/test/html/00_main/index.html");
+        webView.setWebViewClient(new WebViewClientClass());
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
